65c3586ff008e4d4958779711a4b8a21e610f642
LiangYiLuo <1658639790@qq.com>
2017-09-16 12:23:27 +0800

    搭好架子

---
+++ src/components/Main.js
@@ -0,0 +1,22 @@
+require('normalize.css/normalize.css');
+require('styles/App.css');
+
+import React from 'react';
+import Header from './header.js'
+let yeomanImage = require('../images/yeoman.png');
+
+class AppComponent extends React.Component {
+  render() {
+    return (
+      <div className="index">
+        <img src={yeomanImage} alt="Yeoman Generator" />
+        <Header/>
+      </div>
+    );
+  }
+}
+
+AppComponent.defaultProps = {
+};
+
+export default AppComponent;
---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+import React,{Component} from 'react';
+import '../styles/header.less';
+
+export default class Header extends React.Component{
+  render(){
+    return (
+      <div className="component-header">this is div</div>
+    )
+  }
+}
