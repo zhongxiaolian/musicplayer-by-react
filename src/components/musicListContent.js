0679a6eee76c60fcce3c6238495fdeb9fe43cdcf
LiangYiLuo <1658639790@qq.com>
2017-09-25 22:11:10 +0800

    bug修复

--- src/components/musicListContent.js
+++ src/components/musicListContent.js
@@ -2,6 +2,19 @@ import React,{Component} from "react";
 import MusicListService from "../services/musicListService.js";
 import MusicListItem from "./musicListItem.js";
 
+let startY = 0;
+let moveY = 0;
+let totalMove = 0;
+let musicContent = 0;
+let musicContentHeight = 0;
+let musicContentParent = 0;
+//20是给父盒子加的边框
+let musicContentParentHeight = 0;
+let maxDistance = 100;
+let goTop = 0;
+let goBottom = null;
+
+
 export default class MusicListContent extends Component{
   constructor(){
     super();
@@ -11,13 +24,32 @@ export default class MusicListContent extends Component{
     }
   }
   componentWillReceiveProps(props){
-    if(props.params.musictype){
-      let newMusicList = MusicListService.getMusicList(this.state.musicType);
+    if(props.params.musictype && props.params.musictype !== this.state.musicType){
+      let newMusicList = MusicListService.getMusicList(props.params.musictype);
       this.setState({
         musicList : newMusicList,
         musicType : props.params.musictype
       });
     }
+  }
+  componentDidUpdate(){
+      //把touchmove相关的数据全部重置
+      startY = 0;
+      moveY = 0;
+      totalMove = 0;
+      musicContent = this.refs.musicContent;
+      musicContentHeight = musicContent.offsetHeight;
+      musicContentParent = musicContent.parentElement;
+      //减去20的border
+      musicContentParentHeight = musicContent.parentElement.offsetHeight-20;
+      if(musicContentHeight>musicContentParentHeight){
+        goBottom = -(musicContentHeight-musicContentParentHeight)
+      }else{
+        goBottom = 0;
+      }
+      //关闭过度，每次切换歌单都需要把ul移到最前面
+      this.refs.musicContent.style.transition = "";
+      this.refs.musicContent.style.transform = "translateY("+(0)+"px)";
   }
   // 通过回调函数，点击删除按钮删除条目
   deleteMusic(item){
@@ -34,17 +66,18 @@ export default class MusicListContent extends Component{
     document.addEventListener("touchmove",function(e){
       e.preventDefault();
     })
-    let musicContent = this.refs.musicContent;
-    let musicContentHeight = musicContent.offsetHeight;
-    let musicContentParent = musicContent.parentElement;
-    //20是给父盒子加的边框
-    let musicContentParentHeight = musicContent.parentElement.offsetHeight-20;
-    let startY = 0;
-    let moveY = 0;
-    let maxDistance = 100;
-    let totalMove = 0;
-    let goTop = 0;
-    let goBottom = -(musicContentHeight-musicContentParentHeight);
+
+    musicContent = this.refs.musicContent;
+    musicContentHeight = musicContent.offsetHeight;
+    musicContentParent = musicContent.parentElement;
+    //减去20的border
+    musicContentParentHeight = musicContent.parentElement.offsetHeight-20;
+    if(musicContentHeight>musicContentParentHeight){
+      goBottom = -(musicContentHeight-musicContentParentHeight)
+    }else{
+      goBottom = 0;
+    }
+
     musicContentParent.addEventListener("touchstart",function(e){
       startY = e.touches[0].clientY;
     })
@@ -52,20 +85,37 @@ export default class MusicListContent extends Component{
       //移动的过程中关闭过度效果，否则滑动不流畅
       musicContent.style.transition = "";
       moveY = e.touches[0].clientY-startY;
-      //手指向下滑动，超出最大临界值
-      if(totalMove+moveY>maxDistance){
-        totalMove = maxDistance;
-        musicContent.style.transform = "translateY(" +(totalMove)+"px)";
-        return;
+      // 如果ul的高度大于div的高度
+      if(musicContentHeight>musicContentParentHeight){
+        //手指向下滑动，超出最大临界值
+        if(totalMove+moveY>maxDistance){
+          totalMove = maxDistance;
+          musicContent.style.transform = "translateY(" +(totalMove)+"px)";
+          return;
+        }
+        //手指向上滑动超出最大临界值
+        else if(Math.abs(totalMove+moveY)>(musicContentHeight-musicContentParentHeight+maxDistance)){
+          totalMove = -(musicContentHeight-musicContentParentHeight+maxDistance);
+          musicContent.style.transform = "translateY(" +(totalMove)+"px)";
+          return;
+        }else{
+          musicContent.style.transform = "translateY(" +(totalMove+moveY)+"px)";
+        }
+      }else{//如果ul的高度小于div的高度
+        //手指向下滑动，超出最大临界值
+        console.log(222222)
+        if(totalMove+moveY>maxDistance){
+          totalMove = maxDistance;
+          musicContent.style.transform = "translateY(" +(totalMove)+"px)";
+          return;
+        }else if(totalMove+moveY<0){
+          console.log(12344);
+          musicContent.style.transform = "translateY(" +(0)+"px)";
+        }else{
+          musicContent.style.transform = "translateY(" +(totalMove+moveY)+"px)";
+        }
       }
-      //手指向上滑动超出最大临界值
-      else if(Math.abs(totalMove+moveY)>(musicContentHeight-musicContentParentHeight+maxDistance)){
-        totalMove = -(musicContentHeight-musicContentParentHeight+maxDistance);
-        musicContent.style.transform = "translateY(" +(totalMove)+"px)";
-        return;
-      }else{
-        musicContent.style.transform = "translateY(" +(totalMove+moveY)+"px)";
-      }
+
     })
     musicContentParent.addEventListener("touchend",function(e){
       totalMove = totalMove + moveY;
